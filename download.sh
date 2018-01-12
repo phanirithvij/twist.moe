ac061c741aac7bcd9f2f9e968451ce2159561b9b
Phani Rithvij <phanirithvij2000@gmail.com>
2018-01-12 11:15:06 +0530

    Adding download all option
    Enter 'a' to download all episodes

--- download.sh
+++ download.sh
@@ -1,12 +1,23 @@
 IFS=$'\n'
 l=`cat list.txt | wc -l`
 echo Total no of episodes : $l
+echo Enter \'a\' for all episodes
 echo start at:
 read s
+if [[ $s != "a" ]]
+then
 echo end at:
 read e
+fi
+if [[ "$s" == "a" ||  "$e" == "a" ]]
+then
+for i in $(cat list.txt)
+do
+wget -c -q --show-progress $i
+done
+else
 j=1
-for i in `cat list.txt`
+for i in $(cat list.txt)
 do
 	if [ $j -ge $s -a $j -le $e ]
 	then
@@ -14,3 +25,4 @@ do
 	fi
 	j=$((j+1))
 done
+fi
