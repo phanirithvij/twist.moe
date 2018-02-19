83df51ac88a8d77521d771eb8795813e713d94c3
phanirithvij <phanirithvij20003@gmail.com>
2018-02-19 21:28:56 +0530

    VIdeo src change and lastplayed localstorage implemented

--- search.js
+++ search.js
@@ -1,88 +1,88 @@
-	//Search functions
-	//search.js
-	
-	const seart = document.querySelector('[name=anisearch]');
-	const sears = document.querySelector('[name=kitssearch]');
-	function findMatches(wordToMatch, anime) {
-  		return anime.filter(ani => {
+  //Search functions
+  //search.js
+  
+	const suggestions = document.querySelector('.suggestions');
+  const seart = document.querySelector('[name=anisearch]');
+  const sears = document.querySelector('[name=kitssearch]');
+  function findMatches(wordToMatch, anime) {
+      return anime.filter(ani => {
     
     // here we need to figure out if the name matches what was searched 
     
-    	const regex = new RegExp(wordToMatch, 'gi');
-    	if (ani.alt){
-    	return ani.title.match(regex) || ani.name.match(regex) || ani.alt.match(regex)
-    	}else{
-    		return ani.title.match(regex) || ani.name.match(regex)
-    	}
-  		});
-	}
+      const regex = new RegExp(wordToMatch, 'gi');
+      if (ani.alt){
+      return ani.title.match(regex) || ani.name.match(regex) || ani.alt.match(regex)
+      }else{
+        return ani.title.match(regex) || ani.name.match(regex)
+      }
+      });
+  }
 
-	function twinkle(e){
-		console.log(e);
-		ele = e;
-	}
-
-	function displayMatches(inp,m2) {
-		var sear = '';
-		/*if m2 is not defined
-		search by the value found in this search bar
-		*/
-		if(!m2){
-		sear = seart.value;
-		//console.log("!m2" + sear);
-		}
-		else{
-		//if m2 is found search by m2	
-		sear = m2;
-		
-		}
-  		const matchArray = findMatches(sear, anime);
- 	    const html = matchArray.map(ani => {
-    	const regex = new RegExp(sear, 'gi');
-    	
-    	//aniname is japanese(or name from twist.moe directly)
-    	
-    	const aniName = ani.title;
-    	var engName = '';
-    	
-    	//if there is english name(ani.alt)
-    	
-    	if(ani.alt != false){
-    	engName = ani.alt;
-    	}
-    	
-    	//title is englishname (if not found aniname)
-    	
-    	const title = engName || aniName;
-    	//console.log(title);
-    	return`
-    	<a onmouseover="//twinkle(this)" href="https://twist.moe/a/${ani.name}" title="${title}">
-      	<li>
+  function twinkle(e){
+    console.log(e);
+    ele = e;
+  }
+  function displayMatches(inp,m2) {
+    var sear = '';
+    /*if m2 is not defined
+    search by the value found in this search bar
+    */
+    if(!m2){
+    sear = seart.value;
+    //console.log("!m2" + sear);
+    }
+    else{
+    //if m2 is found search by m2 
+    sear = m2;
+    
+    }
+      const matchArray = findMatches(sear, anime);
+      var html = matchArray.map(ani => {
+      const regex = new RegExp(sear, 'gi');
+      
+      //aniname is japanese(or name from twist.moe directly)
+      
+      const aniName = ani.title;
+      var engName = '';
+      const animetlink ="\'" +  ani.name + "\'";
+      //if there is english name(ani.alt)
+      
+      if(ani.alt != false){
+      engName = ani.alt;
+      }
+      console.log(animetlink,ani.name);
+      //title is englishname (if not found aniname)
+      
+      const title = engName || aniName;
+      //console.log(title);
+      return`
+      <a onclick="changVid(${animetlink})" href="#" title="${title}">
+        <li>
         <span class="name">${aniName}</span><br>
-      	</li>
-      	</a>`;
-  		}).join('');
-  		
-  		/*
-  		IF box is empty and
-  		input didn't come from 
-  		m2(as a second argument)
-		*/
+        </li>
+        </a>`;
+      }).join('');
+      
+      /*
+      If box is empty and
+      input didn't come from 
+      m2(as a second argument from kitsusearch)
+    */
 
-  		if(seart.value != "" && !m2){
-  		//console.log(seart.value != "");
-  		suggestions.innerHTML = html;
-  		//console.log("html" + html);
-  		//console.log(seart.value);
-  		}
-  		else{
-  		toggleSearch();
-  		suggestions.innerHTML += html;
-  		if(suggestions.innerHTML == ""){
+      if(seart.value != "" && !m2){
+      //console.log(seart.value != "");
+      suggestions.innerHTML = html;
+      //console.log("html" + html);
+      //console.log(seart.value);
+      }
+      else{
+      toggleSearch();
+      suggestions.innerHTML += html;
+      if(suggestions.innerHTML == ""){
+          html += '<li><span class="name">No Results Found</span><br></li>';
+      suggestions.innerHTML += html;
+      }
+      }
+      //console.clear();
+  }
 
-  		}
-  		}
-  		//console.clear();
-	}
-
-	const suggestions = document.querySelector('.suggestions');
