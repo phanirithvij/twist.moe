73c9bf0237727627b07f66715075f309c7ed4eb4
phanirithvij <phanirithvij20003@gmail.com>
2018-07-25 21:20:56 +0530

    removed every thing

--- sizes.sh
+++
@@ -1,7 +0,0 @@
-DO THIS FOR GETTING SIZES
-
-for i in $(cat Anime/bleach/list.txt)
-do
-x=$(wget $i --spider --server-response -O - 2>&1 | grep -E '^Length'|cut -d " " -f3 | tr ")" "\0" | tr "(" "\0"  )
-echo "$i $x"
-done
