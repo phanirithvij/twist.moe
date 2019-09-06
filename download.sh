e290fa3a3a5bb24bbd26b0cc27e4f4d00ccabe86
Phani Rithvij <phanirithvij2000@gmail.com>
2019-09-06 12:05:07 +0530

    modified download.sh to also rename anime

--- download.sh
+++ download.sh
@@ -10,11 +10,13 @@ curl_download(){
     # https://stackoverflow.com/a/35019553/8608146
     i=$1
     i=${i%$'\r'}
-    echo "downloading $i"
-    curl -L -O -C - "$i" -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36'
+    echo "downloading $i as $2"
+    curl -L -o $2 -C - "$i" -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36'
 }
 
 wget_or_curl
+anime=$(basename $PWD)
+echo "anime is $anime"
 echo "curl is installed: $CURL_TRUE"
 
 IFS=$'\n'
@@ -34,16 +36,18 @@ else
     s=$1
     e=$2
 fi
+count=0
 if [[ "$s" == "a" ||  "$e" == "a" ]]
 then
     for i in $(cat list.txt)
     do
+        count=$((count+1))
         if [[ $CURL_TRUE == "yes" ]]
         then
             # curl -g -L -O -C - $i
-            curl_download $i
+            curl_download $i $anime-$count.mp4
         else
-            wget -c -q --show-progress $i
+            wget -c -q --show-progress $i -O $anime-$count.mp4
         fi
         echo $i
     done
@@ -55,9 +59,9 @@ else
         then
             if [[ $CURL_TRUE == "yes" ]]
             then
-                curl_download $i
+                curl_download $i $anime-$j.mp4
             else
-                wget -c -q --show-progress $i
+                wget -c -q --show-progress $i -O $anime-$j.mp4
             fi
         fi
         j=$((j+1))
